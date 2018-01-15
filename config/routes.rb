8e0d8e715984bcb0296296ad697ccc8a69169772
vinsonly <vinsonly@live.ca>
2018-01-14 21:08:14 -0800

    generated controller for home, set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  match	':controller(/:action(/:id))',	:via	=>	:get
+  match	':controller(/:action(/:id))',	:via	=>	:post
+
+  root "home/index"
+
 end
