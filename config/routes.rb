c7662147e1eb24bb9d80d4e0aec106e203801b3c
vinsonly <vinsonly@live.ca>
2018-01-14 21:13:04 -0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # resources :admin
+  # resources :home
+  # resources :access
+
+  get 'home/index' => 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   match	':controller(/:action(/:id))',	:via	=>	:get
   match	':controller(/:action(/:id))',	:via	=>	:post
 
-  root "home/index"
+  root "home#index"
 
 end
